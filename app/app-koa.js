137242252c2a07bb9c603b10257aa1907a32f26d
yemiancheng <ymc.github@gmail.com>
2020-01-14 18:51:36 +0800

    refactor:rename server config file

--- app/app-koa.js
+++ app/app-koa.js
@@ -3,24 +3,27 @@
 //----------serve scr----------
 //tasks:
 //
-
 //include some lib
 const Koa = require('koa')
 const fs = require('fs')
 const path = require('path')
-const resolve = file => path.resolve(__dirname, file)
+
 //include some config
-const configServer = require('../config/server')
-//read html index  file
-const template = fs.readFileSync(configServer.index, 'utf-8')
+const config = require('../config/server.config')
+const resolve = file => path.resolve(__dirname, file)
+
+if (process.env.NODE_ENV === undefined) {
+  process.env.NODE_ENV = 'production'
+}
+const isProd = process.env.NODE_ENV === 'production'
 
 const app = new Koa()
 // for static serve in public path
-app.use(require('koa-static')(configServer.static))
+app.use(require('koa-static')((isProd ? config.build.static : config.dev.static)))
 // for static web serve in dist path
 app.use(async (ctx, next) => {
   try {
-    ctx.body = template
+    ctx.body = fs.readFileSync((isProd ? config.build.index : config.dev.index), 'utf-8')
     ctx.set('Content-Type', 'text/html')
     ctx.set('Server', 'Koa2 client side render')
   } catch (e) {
--- app/csr-server.js
+++ app/csr-server.js
@@ -1,7 +1,14 @@
 const app = require('./app-koa')
-const configServer = require('../config/server')
-const port = process.env.PORT || configServer.port || 3000
-app.listen(port, '0.0.0.0', () => {
+const config = require('../config/server.config')
+
+if (process.env.NODE_ENV === undefined) {
+  process.env.NODE_ENV = 'production'
+}
+const isProd = process.env.NODE_ENV === 'production'
+
+const host = process.env.HOST || (isProd ? config.build.host : config.dev.host) || 'localhost'
+const port = process.env.PORT || (isProd ? config.build.port : config.dev.port) || 3000
+app.listen(port, host, () => {
   console.log(`server started at localhost:${port}`)
 }).on('listening', () => console.log('serve is running'))
   .on('error', err => console.log(err))
